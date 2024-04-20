24dabd1387e80631eb17a847f0ffcd9f3693da32
NathanHandley <nathanhandley@protonmail.com>
2024-04-19 20:54:21 -0500

    Shell out the EQ asset conditioner (checkpoint)

---
+++ EQWOWConverter/src/EQAssetConditioner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQWOWConverter
+{
+    internal class EQAssetConditioner
+    {
+        public EQAssetConditioner() { }
+
+        public bool CondenseObjects(string eqExportsRawPath, string eqExportsCondensedPath)
+        {
+            Console.WriteLine("Condensing Objects Started...");
+
+            // Make sure the raw path exists
+            if (Directory.Exists(eqExportsRawPath) == false)
+            {
+                Console.WriteLine("ERROR - Raw input path of '" + eqExportsRawPath + "' does not exist.");
+                Console.WriteLine("Condensing Objects Ended (Failed)");
+                return false;
+            }
+
+            // Create base folder
+            Directory.CreateDirectory(eqExportsCondensedPath);
+
+            // Delete/Recreate the objects subfolder
+            string objectsOutputFolder = Path.Combine(eqExportsCondensedPath, "objects");
+            if (Directory.Exists(objectsOutputFolder))
+                Directory.Delete(objectsOutputFolder, true);
+            Directory.CreateDirectory(objectsOutputFolder);
+
+            // Iterate through each zone folder
+            string[] zoneDirectories = Directory.GetDirectories(eqExportsRawPath);
+            foreach (string zoneDirectory in zoneDirectories)
+            {
+                // Folder is the zone name
+                string zoneName = zoneDirectory.Split('\\').Last();
+                Console.WriteLine(" - Processing objects for zone '" + zoneName + "'");
+
+                // Check for objects folder
+                string zoneObjectFolder = Path.Combine(zoneDirectory, "Objects");
+                if (Directory.Exists(zoneObjectFolder) == false)
+                {
+                    Console.WriteLine(" - No objects in zone, skipping to next");
+                    continue;
+                }
+
+                // Iterate through the Objects by name (minus extension)
+                // TODO: HERE
+            }
+
+            Console.WriteLine("Condensing Objects Ended (Success)");
+            return true;
+        }
+    }
+}
---
+++ EQWOWConverter/src/Program.cs
@@ -0,0 +1,61 @@
+﻿// Author: Nathan Handley (https://github.com/NathanHandley), 2024
+// Released under GPLv3, unless otherwise noted in specific code files
+
+using EQWOWConverter;
+
+internal class Program
+{
+    // TODO: Move to config
+    public static string CONFIG_PATH_EQEXPORTSRAW       = "E:\\Development\\EQWOW-Reference\\Working\\Assets\\EQExports";
+    public static string CONFIG_PATH_EQEXPORTSCONDITIONED = "E:\\Development\\EQWOW-Reference\\Working\\Assets\\EQExportsConditioned";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("###### EQ WOW Converter ######");
+        bool doLoopForCommands = true;
+        while (doLoopForCommands == true)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine(" [1] - Condition Exported EQ Data");
+            Console.WriteLine(" [X] - Exit");
+            Console.Write("Command: ");
+            string? enteredCommand = Console.ReadLine();
+            if (enteredCommand == null)
+            {
+                Console.WriteLine("Enter a command");
+            }
+            else
+            {
+                switch (enteredCommand.ToUpper())
+                {
+                    case "X":
+                        {
+                            Console.WriteLine("Exiting.");
+                            doLoopForCommands = false;
+                        }
+                        break;
+                    case "1":
+                        {
+                            Console.WriteLine("Conditioning Exported EQ Data...");
+                            EQAssetConditioner condenser = new EQAssetConditioner();
+                            bool condenseObjectsResult = condenser.CondenseObjects(CONFIG_PATH_EQEXPORTSRAW, CONFIG_PATH_EQEXPORTSCONDITIONED);
+                            if (condenseObjectsResult == false)
+                            {
+                                Console.WriteLine("Exported EQ Data Conditioning Failed.");
+                                break;
+                            }                            
+                            Console.WriteLine("Exported EQ Data Conditioning Succeeded.");
+                        } break;
+                    default:
+                        {
+                            Console.WriteLine("Unknown Command");
+                        }
+                        break;
+                }
+            }
+        }
+        Console.WriteLine("Press any key to quit...");
+        Console.ReadKey();
+    }
+}
