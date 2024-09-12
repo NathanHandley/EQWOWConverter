5b2b653865b1e5301f4ca4d78435da939632d2e2
NathanHandley <nathanhandley@protonmail.com>
2024-09-12 08:32:49 -0500

    Add music areas to Befallen

--- EQWOWConverter/Zones/Properties/Classic/BefallenZoneProperties.cs
+++ EQWOWConverter/Zones/Properties/Classic/BefallenZoneProperties.cs
@@ -17,6 +17,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,10 +27,34 @@ namespace EQWOWConverter.Zones.Properties
     {
         public BefallenZoneProperties() : base()
         {
+            // Cave Sound - Zone Wide
             SetBaseZoneProperties("befallen", "Befallen", 35.22f, -75.27f, 2.19f, 0, ZoneContinentType.Antonica);
-            //AddValidMusicInstanceTrackIndexes(0, 1, 2, 3, 4, 5, 6, 7);
             SetZonewideEnvironmentAsIndoors(0, 0, 60, ZoneFogType.Heavy, 130, 130, 130);
             OverrideVertexColorIntensity(0.6);
+            //AddValidMusicInstanceTrackIndexes(0, 1, 2, 3, 4, 5, 6, 7);
+            AddZoneArea("Entry", 54.481789f, 55.857861f, 37.370251f, -124.496246f, -144.035538f, -5.204370f, "befallen-06", "befallen-06");
+
+
+
+            AddZoneArea("Entry", 26.557690f, 11.077470f, 20.215931f, -203.251007f, -90.467781f, -4.588550f, "befallen-06", "befallen-06");
+            AddZoneArea("Well", -262.989471f, 98.996452f, 7.501510f, -346.584534f, 69.684769f, -16.758711f, "befallen-00", "befallen-00");
+            AddZoneArea("Well", -337.689789f, 116.707527f, 7.654700f, -406.665894f, 54.447369f, -15.360780f, "befallen-00", "befallen-00");
+            AddZoneArea("Well", -346.760834f, 116.179741f, -50.434662f, -411.030212f, 49.695049f, -76.359451f, "befallen-00", "befallen-00");
+            AddZoneArea("Bottom South", -785.887939f, 193.538437f, -28.172581f, -1011.797668f, -113.528923f, -76.708099f, "befallen-07", "befallen-07");
+            AddZoneArea("Bottom South", -791.871948f, -177.910599f, -28.172581f, -1011.797668f, -248.485245f, -76.708099f, "befallen-07", "befallen-07");
+            AddZoneArea("Bottom South", -804.666870f, 193.538437f, -28.172581f, -1011.797668f, -248.485245f, -76.708099f, "befallen-07", "befallen-07");
+            AddZoneArea("Bottom South", -782.485229f, -111.742867f, -28.172581f, -860.202637f, -176.478546f, -70.821602f, "befallen-07", "befallen-07");
+            AddZoneArea("Bottom South", -782.741333f, -14.617890f, -28.172581f, -800.851929f, -111.868011f, -76.708099f, "befallen-07", "befallen-07");
+            AddZoneArea("Middle West", -584.371948f, -26.771641f, -41.967979f, -598.573914f, -45.613731f, -47.362438f, "befallen-01", "befallen-01"); // Small recess that needs to grab for Middle West first
+            AddZoneArea("Bottom Center", -629.656189f, 160.041992f, -23.937210f, -806.572266f, -202.661743f, -89.750160f, "befallen-04", "befallen-04");
+            AddZoneArea("Bottom Center", -531.246948f, -12.537450f, -42.361931f, -634.891602f, -71.659630f, -83.953720f, "befallen-04", "befallen-04"); // NW room with rock that goes through floor above
+            AddZoneArea("Bottom Center", -514.774353f, -147.121918f, -42.216499f, -645.313721f, -209.536377f, -81.840134f, "befallen-04", "befallen-04"); // NE stair room
+            AddZoneArea("Bottom North", -291.817841f, 133.591080f, -50.768059f, -629.886292f, -214.062393f, -92.168129f, "befallen-05", "befallen-05");
+            AddZoneArea("Middle West", -357.848206f, 199.227127f, -23.644470f, -605.808105f, -13.567110f, -45.856098f, "befallen-01", "befallen-01");
+            AddZoneArea("Middle West", -465.827972f, 199.227127f, -23.644470f, -605.808105f, -74.008232f, -45.856098f, "befallen-01", "befallen-01");
+            AddZoneArea("Middle East", -318.861420f, -12.284030f, -23.644470f, -560.344482f, -304.230774f, -45.856098f, "befallen-03", "befallen-03");
+            AddZoneArea("Middle East", -284.659149f, -253.402206f, -15.765840f, -414.804291f, -300f, -41.967731f, "befallen-03", "befallen-03");
+            AddZoneArea("Top", 50.491291f, 281.136414f, 31.637880f, -410.858093f, -301.009155f, -28.019581f, "befallen-02", "befallen-02");
             AddZoneLineBox("commons", -1155.6317f, 596.3344f, -42.280308f, ZoneLineOrientationType.North, -49.9417f, 42.162197f, 12.469f, -63.428658f, 27.86666f, -0.5000006f);
         }
     }
