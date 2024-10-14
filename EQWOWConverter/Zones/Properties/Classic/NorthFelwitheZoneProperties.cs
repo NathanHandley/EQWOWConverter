ec483b3b15cf92a6e3ec20ed98f08a68b5815892
NathanHandley <nathanhandley@protonmail.com>
2024-10-14 13:13:02 -0500

    Add ambience and music to Felwithe

--- EQWOWConverter/Zones/Properties/Classic/NorthFelwitheZoneProperties.cs
+++ EQWOWConverter/Zones/Properties/Classic/NorthFelwitheZoneProperties.cs
@@ -27,8 +27,21 @@ namespace EQWOWConverter.Zones.Properties
     {
         public NorthFelwitheZoneProperties() : base()
         {
+            // TODO: More zone areas
             SetBaseZoneProperties("felwithea", "Northern Felwithe", 94f, -25f, 3.75f, 0, ZoneContinentType.Faydwer);
-            //AddValidMusicInstanceTrackIndexes(0, 1, 2);
+            Enable2DSoundInstances("wind_lp2", "wind_lp3", "wind_lp4");
+            SetZonewideMusic("felwithea-00", "felwithea-00", Configuration.CONFIG_AUDIO_MUSIC_DEFAULT_VOLUME, true);
+
+            AddZoneArea("Entry", "felwithea-01", "felwithea-01", Configuration.CONFIG_AUDIO_MUSIC_DEFAULT_VOLUME, false);
+            AddZoneAreaBox("Entry", 117.707916f, 462.981293f, 186.264618f, -86.893219f, 30.036381f, -100f);
+
+            if (Configuration.CONFIG_AUDIO_USE_ALTERNATE_TRACKS == false)
+            {
+                AddZoneArea("Water Outlook", "felwithea-02", "felwithea-02");
+                AddZoneAreaBox("Water Outlook", 392.955811f, -536.260681f, 44.567032f, 74.084091f, -926.284851f, -77.346931f);
+                AddZoneAreaBox("Water Outlook", 122.281776f, -638.404175f, 3.277470f, -162.649353f, -939.232300f, -119.733437f);
+            }
+
             SetZonewideEnvironmentAsOutdoorsWithSky(58, 75, 58, ZoneFogType.Medium, 0.5f, 1f);
             AddZoneLineBox("felwitheb", 251.268646f, -832.815125f, -13.999020f, ZoneLineOrientationType.North, 364.650452f, -711.921509f, -1.531000f, 342.316345f, -727.911865f, -14.499750f);
             AddZoneLineBox("gfaydark", -1931.678101f, -2613.879639f, 20.406450f, ZoneLineOrientationType.West, 56.161152f, 242.410782f, 26.469000f, 27.806530f, 193.596893f, -0.500000f);
--- EQWOWConverter/Zones/Properties/Classic/SouthFelwitheZoneProperties.cs
+++ EQWOWConverter/Zones/Properties/Classic/SouthFelwitheZoneProperties.cs
@@ -27,9 +27,15 @@ namespace EQWOWConverter.Zones.Properties
     {
         public SouthFelwitheZoneProperties() : base()
         {
+            // TODO: Add more zone areas
             // Bug: Hole in the doorway into the teleport room (bottom).  Might be scale related -- It's also this way on live!  Test after collision changes.
             SetBaseZoneProperties("felwitheb", "Southern Felwithe", -790f, 320f, -10.25f, 0, ZoneContinentType.Faydwer);
             SetZonewideEnvironmentAsOutdoorsWithSky(58, 75, 58, ZoneFogType.Medium, 0.5f, 1f);
+
+            // This track normally plays in north felwithe
+            if (Configuration.CONFIG_AUDIO_USE_ALTERNATE_TRACKS == true)
+                SetZonewideMusic("felwithea-02", "felwithea-02", Configuration.CONFIG_AUDIO_MUSIC_DEFAULT_VOLUME, true);
+
             AddZoneLineBox("felwithea", 336.521210f, -720.996582f, -13.999750f, ZoneLineOrientationType.South, 245.892227f, -825.463867f, -1.531000f, 218.101257f, -839.849731f, -14.500020f);
             AddTeleportPad("felwitheb", 503.797028f, -496.463074f, -5.001940f, ZoneLineOrientationType.West, 435.755615f, -584.819824f, 31.000059f, 6.0f);
             AddTeleportPad("felwitheb", 303.943054f, -581.065125f, -13.999980f, ZoneLineOrientationType.North, 503.520294f, -338.805695f, 3.999970f, 6.0f);
