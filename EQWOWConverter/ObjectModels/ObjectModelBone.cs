568e6cef4e7df6af9098223b05465a67469545f8
NathanHandley <nathanhandley@protonmail.com>
2024-11-17 18:24:15 -0600

    Add some core bone crc assignments

--- EQWOWConverter/ObjectModels/ObjectModelBone.cs
+++ EQWOWConverter/ObjectModels/ObjectModelBone.cs
@@ -25,16 +25,43 @@ namespace EQWOWConverter.ObjectModels
 {
     internal class ObjectModelBone
     {
-        public string BoneNameEQ = string.Empty;
+        private string _BoneNameEQ = string.Empty;
+        public string BoneNameEQ
+        {
+            get { return _BoneNameEQ; }
+            set
+            {
+                _BoneNameCRC = GetCRCValue(value);
+                _BoneNameEQ = value; 
+            }
+        }
+
+        public UInt32 _BoneNameCRC = 0;
+        public UInt32 BoneNameCRC
+        {
+            get { return _BoneNameCRC; }
+        }
+
         public string ParentBoneNameEQ = string.Empty;
         public Int32 KeyBoneID = Convert.ToInt32(KeyBoneType.None);
         public ObjectModelBoneFlags Flags = ObjectModelBoneFlags.Transformed;
         public Int16 ParentBone = -1; // Why is this Int16 instead of Int32?
         public UInt16 SubMeshID = 0;
-        public UInt32 BoneNameCRC = 0;
         public ObjectModelTrackSequences<Vector3> TranslationTrack = new ObjectModelTrackSequences<Vector3>();
         public ObjectModelTrackSequences<QuaternionShort> RotationTrack = new ObjectModelTrackSequences<QuaternionShort>();
         public ObjectModelTrackSequences<Vector3> ScaleTrack = new ObjectModelTrackSequences<Vector3>();
         public Vector3 PivotPoint = new Vector3();
+
+        private UInt32 GetCRCValue(string name)
+        {
+            switch (name)
+            {
+                case "main":    return 521822810;
+                case "root":    return 3066451557;
+                case "name":    return 2738135331;
+                case "breath":  return 3299126614;
+                default:        return 0;
+            }
+        }
     }
 }
