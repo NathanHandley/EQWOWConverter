9ffe5b97d33fff47f581fd1c63393e1ff14ca2ca
NathanHandley <nathanhandley@protonmail.com>
2025-02-11 08:40:00 -0600

    Enable editing of existing FactionDBC rows

--- EQWOWConverter/Files/WOWFiles/DBC/FactionDBC.cs
+++ EQWOWConverter/Files/WOWFiles/DBC/FactionDBC.cs
@@ -87,5 +87,75 @@ namespace EQWOWConverter.WOWFiles
             newRow.AddStringLang(creatureFaction.Description); // Description
             Rows.Add(newRow);
         }
+
+        protected override void OnPostLoadDataFromDisk()
+        {
+            // Convert any raw data rows to actual data rows (which should be all of them)
+            foreach(DBCRow row in Rows)
+            {
+                // This shouldn't be possible, but control for it just in case
+                if (row.SourceRawBytes.Count == 0)
+                {
+                    Logger.WriteError("FactionDBC had no source raw bytes when converting a row in OnPostLoadDataFromDisk");
+                    continue;
+                }
+
+                // Fill every field
+                int byteCursor = 0;
+                row.AddIntFromSourceRawBytes(ref byteCursor); // ID
+                row.AddIntFromSourceRawBytes(ref byteCursor); // ReputationIndex
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Race Mask 1
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Race Mask 2
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Race Mask 3
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Race Mask 4
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Class Mask 1
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Class Mask 2
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Class Mask 3
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Class Mask 4
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Base 1
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Base 2
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Base 3
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Base 4
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Flags 1
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Flags 2
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Flags 3
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Reputation Flags 4
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Parent Faction ID
+                row.AddFloatFromSourceRawBytes(ref byteCursor); // Parent Faction Mod 1
+                row.AddFloatFromSourceRawBytes(ref byteCursor); // Parent Faction Mod 2
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Parent Faction Cap 1
+                row.AddIntFromSourceRawBytes(ref byteCursor); // Parent Faction Cap 2
+                row.AddStringLangFromSourceRawBytes(ref byteCursor, StringBlock); // Name
+                row.AddStringLangFromSourceRawBytes(ref byteCursor, StringBlock); // Description
+
+                // Purge raw data
+                row.SourceRawBytes.Clear();
+            }
+
+            // Remove reputationindex from specific rows for reuse
+            foreach(DBCRow row in Rows)
+            {
+                // Fields to clear are as follows (reputation index):
+                // "Wildhammer Clan" (8)
+                // "Caer Darrow" (34)
+                // "REUSE" (82)
+                // "Test Faction 1" (85)
+                // "Test Faction 2" (86)
+                // "Test Faction 3" (87)
+                // "Tranquillen Conversion" (100)
+                // "Wintersaber Conversion" (101)
+                // "Silver Covenant Conversion" (102)
+                // "Sunreavers Conversion" (103)
+
+                // Clear out specific rep index fields
+                DBCRow.DBCFieldInt repIndexField = (DBCRow.DBCFieldInt)row.AddedFields[1];
+                if (repIndexField.Value == 8 || repIndexField.Value == 34 || repIndexField.Value == 82 || repIndexField.Value == 85
+                    || repIndexField.Value == 86 || repIndexField.Value == 87 || repIndexField.Value == 100
+                    || repIndexField.Value == 101 || repIndexField.Value == 102 || repIndexField.Value == 103)
+                { 
+                    repIndexField.Value = -1;
+                }
+            }
+        }
     }
 }
