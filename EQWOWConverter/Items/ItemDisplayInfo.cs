7b1190991f8c3c5f55a613c50611d62c6476cd9e
NathanHandley <nathanhandley@protonmail.com>
2025-03-28 07:41:12 -0500

    Add flowing black robe graphics

--- EQWOWConverter/Items/ItemDisplayInfo.cs
+++ EQWOWConverter/Items/ItemDisplayInfo.cs
@@ -127,7 +127,7 @@ namespace EQWOWConverter.Items
             {
                 // Generate the robe geometry, if needed
                 int robeID = materialTypeID - 9;
-                if (robeID != 4 && robeID != 1)
+                if (robeID != 4 && robeID != 1 && robeID != 2)
                     robeID = 7;
                 BuildAndCopyTexturesForRobe(robeID);
                 string robeIDString = "0" + robeID.ToString();
