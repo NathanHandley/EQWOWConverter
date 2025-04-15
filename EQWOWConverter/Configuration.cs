d71a0bfff38b0ce5632c1ad4d7fc58ffeec3137f
NathanHandley <nathanhandley@protonmail.com>
2025-04-15 07:02:33 -0500

    Add start locations data for race/class combos
    Not yet wired up

--- EQWOWConverter/Configuration.cs
+++ EQWOWConverter/Configuration.cs
@@ -94,7 +94,7 @@ namespace EQWOWConverter
         // ====================================================================
         // The value EQ vertices multiply by when translated into WOW vertices
         // A WORLD_SCALE value of 0.25 seems to be 1:1 with EQ.  0.28 allows humans and 0.4 allows taurens to enter rivervale bank door
-        public static readonly float GENERATE_WORLD_SCALE = 0.255f; //0.29f;
+        public static readonly float GENERATE_WORLD_SCALE = 0.255f;
         public static readonly float GENERATE_CREATURE_SCALE = 0.255f;
         public static readonly float GENERATE_EQUIPMENT_HELD_SCALE = 0.35f;
 
