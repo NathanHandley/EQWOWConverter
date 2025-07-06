c47d1d50cc89efee3ac036a7dc43d180368f79c2
NathanHandley <nathanhandley@protonmail.com>
2025-07-06 16:29:20 -0500

    Read all the Spells.eff data

--- EQWOWConverter/Files/EQFiles/EQSpellsEFF.cs
+++ EQWOWConverter/Files/EQFiles/EQSpellsEFF.cs
@@ -1,31 +1,31 @@
-﻿using EQWOWConverter.Common;
+﻿//  Author: Nathan Handley (nathanhandley@protonmail.com)
+//  Copyright (c) 2025 Nathan Handley
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using EQWOWConverter.Common;
 
 namespace EQWOWConverter.EQFiles
 {
-    //  Author: Nathan Handley (nathanhandley@protonmail.com)
-    //  Copyright (c) 2025 Nathan Handley
-    //
-    //  This program is free software: you can redistribute it and/or modify
-    //  it under the terms of the GNU General Public License as published by
-    //  the Free Software Foundation, either version 3 of the License, or
-    //  (at your option) any later version.
-    //
-    //  This program is distributed in the hope that it will be useful,
-    //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-    //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    //  GNU General Public License for more details.
-    //
-    //  You should have received a copy of the GNU General Public License
-    //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
     // Much of this structure data was from research from "Stolistic" on the Project Latern discord
     internal class EQSpellsEFF
     {
         internal class SectionData
         {
             public string[] SpriteNames = new string[3];
-            public string Type = string.Empty;
-            public int[] LocationIDs = new int[3]; // 0 = Body Center, 1 = Head, 2 = Right Hand, 3 = Left Hand, 4 = Right Foot, 5 = Left Foot, 6 = Also center of body
+            public string TypeString = string.Empty;
+            public int[] LocationIDs = new int[3]; // -1 = None(?), 0 = Body Center, 1 = Head, 2 = Right Hand, 3 = Left Hand, 4 = Right Foot, 5 = Left Foot, 6 = Also center of body
             public int[] EmissionTypeIDs = new int[3]; // -1 = None, 0 = Hands, 1 = Cone to the right, 2 = Sphere around player, 3 = Disc on the ground, 4 = Column from the ground, 5 = Disc at the player center
             public string[] SpriteListNames = new string[12]; // Up to 12 sprites used for animating particles. Loop through until the end and start at begining. Porticles oriented in a circle, with each 30 degrees from the next for a total of 360 degrees.
             public int SpriteListEffect; // 1 = Projectile, 2 = Disc pulsating outward and then inward
@@ -44,10 +44,61 @@ namespace EQWOWConverter.EQFiles
             public int[] UnknownData = new int[9];  // 9 unknown values that is always zero
             public float[] SpriteListUnknown = new float[12]; // Unsure.  Can be 10, 15, 20, or 30
             public short[] SpriteListCircularShifts = new short[12]; // How many rotational shifts particles will travel during animation.  15 shifts is one full rotation counterclockwise.  Negative goes clockwise.  Ignored if movement is stationary.
-            public short[] SpriteListVerticalForcees = new short[12]; // Verticle force applied to the particles.  -3 and +3 are common.  3 = Upper Top, 2 = Upper Middle, 1 = Upper Bottom, 0 = Player Center, -1 = Lower Top, -2 = Lower Middle, -3 = Lower Bottom
+            public short[] SpriteListVerticalForces = new short[12]; // Verticle force applied to the particles.  -3 and +3 are common.  3 = Upper Top, 2 = Upper Middle, 1 = Upper Bottom, 0 = Player Center, -1 = Lower Top, -2 = Lower Middle, -3 = Lower Bottom
             public float[] SpriteListRadii = new float[12]; // Radius from playre's center for the particles
             public short[] SpriteListMovements = new short[12]; // Determines if the particle moves.  1 = Stationary, 2 = Moves
             public float[] SpriteListScales = new float[12]; // The scale of the particles
+        
+            public void LoadFromBytes(List<byte> bytes, ref int byteCursor)
+            {
+                for (int i = 0; i < 3; i++)
+                    SpriteNames[i] = ByteTool.ReadStringFromBytes(bytes, ref byteCursor, 32);
+                TypeString = ByteTool.ReadStringFromBytes(bytes, ref byteCursor, 32);
+                for (int i = 0; i < 3; i++)
+                    LocationIDs[i] = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmissionTypeIDs[i] = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListNames[i] = ByteTool.ReadStringFromBytes(bytes, ref byteCursor, 32);
+                SpriteListEffect = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                Flags = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterColors[i] = ByteTool.ReadColorBGRAFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterGravities[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                {
+                    EmitterSpawnXs[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                    EmitterSpawnYs[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                    EmitterSpawnZs[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                }
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnRadiuses[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnAngles[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnLifespans[i] = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnVelocities[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnRates[i] = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 3; i++)
+                    EmitterSpawnScale[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 9; i++)
+                    UnknownData[i] = ByteTool.ReadInt32FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListUnknown[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListCircularShifts[i] = ByteTool.ReadInt16FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListVerticalForces[i] = ByteTool.ReadInt16FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListRadii[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListMovements[i] = ByteTool.ReadInt16FromBytes(bytes, ref byteCursor);
+                for (int i = 0; i < 12; i++)
+                    SpriteListScales[i] = ByteTool.ReadFloatFromBytes(bytes, ref byteCursor);
+            }
         }
 
         internal class SpellEffect
@@ -57,20 +108,39 @@ namespace EQWOWConverter.EQFiles
             public SectionData[] SectionDatas = new SectionData[3]; // Always 3, sometimes blank
         }        
 
+        public List<SpellEffect> SpellEffects = new List<SpellEffect>();
+
         public bool LoadFromDisk(string fileFullPath)
         {
-            Logger.WriteDebug(" - Reading Sound Effects Data from '" + fileFullPath + "'...");
+            Logger.WriteDebug(" - Reading Sound Effects Data from '", fileFullPath, "'...");
             if (File.Exists(fileFullPath) == false)
             {
-                Logger.WriteError("- Could not find Sound Effects file that should be at '" + fileFullPath + "'");
+                Logger.WriteError("- Could not find Sound Effects file that should be at '", fileFullPath, "'");
                 return false;
             }
 
             // Load in all the data
             List<byte> fileBytes = FileTool.GetFileBytes(fileFullPath);
 
+            //  There are 255 spell effects in the file, but looks like only 52 (0-51) are populated with anything
+            int byteCursor = 0;
+            for (int i = 0; i < 255; i++)
+            {
+                SpellEffect curEffect = new SpellEffect();
+                curEffect.Field01 = ByteTool.ReadInt32FromBytes(fileBytes, ref byteCursor);
+                curEffect.Field02 = ByteTool.ReadInt32FromBytes(fileBytes, ref byteCursor);
 
+                // Always three sections
+                for (int j = 0; j < 3; j++)
+                {
+                    curEffect.SectionDatas[j] = new SectionData();
+                    curEffect.SectionDatas[j].LoadFromBytes(fileBytes, ref byteCursor);
+                }
 
+                SpellEffects.Add(curEffect);
+            }
+
+            Logger.WriteDebug(" - Done reading Sound Effects from '", fileFullPath, "'");
             return true;
         }
     }
---
+++ EQWOWConverter/Tools/ByteTool.cs
@@ -0,0 +1,116 @@
+﻿//  Author: Nathan Handley (nathanhandley@protonmail.com)
+//  Copyright (c) 2025 Nathan Handley
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using EQWOWConverter.Common;
+using System.Text;
+
+namespace EQWOWConverter
+{
+    internal class ByteTool
+    {
+        public static short ReadInt16FromBytes(List<byte> sourceBytes, ref int byteCursor, bool doAdvanceCursor = true)
+        {
+            if (byteCursor + 2 >= sourceBytes.Count)
+            {
+                Logger.WriteError("ReadInt16FromBytes error, value would be out of bounds");
+                return 0;
+            }
+            byte[] intBytes = new byte[2];
+            for (int i = 0; i < 2; i++)
+                intBytes[i] = sourceBytes[byteCursor + i];
+
+            if (doAdvanceCursor == true)
+                byteCursor += 2;
+
+            return BitConverter.ToInt16(intBytes, 0);
+        }
+
+        public static int ReadInt32FromBytes(List<byte> sourceBytes, ref int byteCursor, bool doAdvanceCursor = true)
+        {
+            if (byteCursor + 4 >= sourceBytes.Count)
+            {
+                Logger.WriteError("ReadInt32FromBytes error, value would be out of bounds");
+                return 0;
+            }
+            byte[] intBytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+                intBytes[i] = sourceBytes[byteCursor + i];
+            
+            if (doAdvanceCursor == true)
+                byteCursor += 4;
+
+            return BitConverter.ToInt32(intBytes, 0);
+        }
+
+        public static float ReadFloatFromBytes(List<byte> sourceBytes, ref int byteCursor, bool doAdvanceCursor = true)
+        {
+            if (byteCursor + 4 >= sourceBytes.Count)
+            {
+                Logger.WriteError("ReadFloatFromBytes error, value would be out of bounds");
+                return 0;
+            }
+            byte[] floatBytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+                floatBytes[i] = sourceBytes[byteCursor + i];
+
+            if (doAdvanceCursor == true)
+                byteCursor += 4;
+
+            return BitConverter.ToSingle(floatBytes, 0);
+        }
+
+        public static string ReadStringFromBytes(List<byte> sourceBytes, ref int byteCursor, int maxStringLength, bool doAdvanceCursor = true)
+        {
+            if (byteCursor + maxStringLength >= sourceBytes.Count)
+            {
+                Logger.WriteError("ReadStringFromBytes error, value would be out of bounds");
+                return string.Empty;
+            }
+            int byteEndIndex = byteCursor + maxStringLength;
+            int firstNullIndex = sourceBytes.IndexOf(0, byteCursor, Math.Min(maxStringLength, sourceBytes.Count - byteCursor));
+            if (firstNullIndex == -1 || firstNullIndex >= byteEndIndex)
+                firstNullIndex = byteEndIndex;
+            string extractedString = Encoding.ASCII.GetString(sourceBytes.GetRange(byteCursor, firstNullIndex - byteCursor).ToArray());
+            
+            if (doAdvanceCursor == true)
+                byteCursor += maxStringLength;
+
+            return extractedString;
+        }
+
+        public static ColorRGBA ReadColorBGRAFromBytes(List<byte> sourceBytes, ref int byteCursor, bool doAdvanceCursor = true)
+        {
+            if (byteCursor + 4 >= sourceBytes.Count)
+            {
+                Logger.WriteError("ReadColorBGRAFromBytes error, value would be out of bounds");
+                return new ColorRGBA();
+            }
+            byte[] intBytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+                intBytes[i] = sourceBytes[byteCursor + i];
+            ColorRGBA colorRGBA = new ColorRGBA();
+            colorRGBA.B = sourceBytes[0];
+            colorRGBA.G = sourceBytes[1];
+            colorRGBA.R = sourceBytes[2];
+            colorRGBA.A = sourceBytes[3];
+
+            if (doAdvanceCursor == true)
+                byteCursor += 4;
+
+            return colorRGBA;
+        }
+    }
+}
