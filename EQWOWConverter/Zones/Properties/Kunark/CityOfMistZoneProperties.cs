737aacde6cd91b923872d1805831a76bd0ea4aad
NathanHandley <nathanhandley@protonmail.com>
2025-12-08 08:51:29 -0600

    Add water to City of Mist

--- EQWOWConverter/Zones/Properties/Kunark/CityOfMistZoneProperties.cs
+++ EQWOWConverter/Zones/Properties/Kunark/CityOfMistZoneProperties.cs
@@ -14,12 +14,6 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EQWOWConverter.Zones.Properties
 {
     internal class CityOfMistZoneProperties : ZoneProperties
@@ -28,8 +22,15 @@ namespace EQWOWConverter.Zones.Properties
         {
             // TODO: Any in-zone teleports?
             // TODO: There is a lift "CMLIFT101".  See Doors table
-            AddZoneLineBox("emeraldjungle", 0.121500f, -774.691650f, 0.000000f, ZoneLineOrientationType.West,
-                309.691193f, -1730.243408f, -300.343506f, 291.030334f, -1789.959473f, -335.468658f);
+            AddZoneLineBox("emeraldjungle", 0.121500f, -774.691650f, 0.000000f, ZoneLineOrientationType.West, 309.691193f, -1730.243408f, -300.343506f, 291.030334f, -1789.959473f, -335.468658f);
+
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 181.788925f, 434.332367f, -168.714798f, 123.955620f, -17.999960f, 75); // Water around westmost building
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 62.186371f, -87.708946f, -113.876907f, -366.743774f, -35.999989f, 100f); // Water in the middle building (ring of water) and some eastbound tunnel
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 123.399017f, -271.990479f, -121.743713f, -742.099121f, -17.999990f, 100f); // Entry water and first section in, and some of the tunnels between
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 280.146210f, -503.799011f, 115.423653f, -655.627441f, -5.999990f, 100f); // Northeast room with incline water, bottom water part and tunnels
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 227.588181f, -399.097443f, 169.080063f, -419.527283f, 36.000019f, 11f); // Northeast room with incline water, top big rect
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "t75_agua1", 218.242523f, -418.708801f, 177.775513f, -431.825958f, 36.000019f, 11f); // Northeast room with incline water, top small rect
+            AddLiquidPlane(ZoneLiquidType.Water, "t75_agua1", 217.685974f, -431.825958f, 178.617783f, -503.799011f, 36.000019f, 0.014160f, ZoneLiquidSlantType.WestHighEastLow, 200f); // Northeast room with incline water, slanted water fall
         }
     }
 }
