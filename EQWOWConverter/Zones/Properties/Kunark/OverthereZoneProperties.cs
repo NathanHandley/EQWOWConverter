f2806430427b70400d56626413ab7f863b897634
NathanHandley <nathanhandley@protonmail.com>
2025-12-18 07:03:30 -0600

    Add liquid to Overthere

--- EQWOWConverter/Zones/Properties/Kunark/OverthereZoneProperties.cs
+++ EQWOWConverter/Zones/Properties/Kunark/OverthereZoneProperties.cs
@@ -14,19 +14,15 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EQWOWConverter.Zones.Properties
 {
     internal class OverthereZoneProperties : ZoneProperties
     {
         public OverthereZoneProperties() : base()
         {
-
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "d_w1ot", 2079.233643f, -2777.568115f, 1818.492432f, -2979.514648f, -42.593590f, 30f); // Mud puddle (east)
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "d_w1ot", -858.570618f, 2560.836914f, -1082.936523f, 2292.712646f, -58.593769f, 30f); // Mud puddle (west)
+            AddLiquidPlaneZLevel(ZoneLiquidType.Water, "d_w1", 11259.541992f, 5907.908691f, 2288.494873f, -5216.059082f, -181.781113f, 500f); // Ocean
         }
     }
 }
